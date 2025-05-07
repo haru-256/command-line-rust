dc6e7e032b6ef7202b3daa6d449d11cce58d5cb9
Yohei Okabayashi <yohei.kuro48@gmail.com>
2025-05-07 21:58:47 +0900

    UPDATE: enhance argument parsing in fortuner by adding sources, pattern, and seed options; improve regex handling and error reporting

--- workbench/12_fortuner/src/lib.rs
+++ workbench/12_fortuner/src/lib.rs
@@ -1,9 +1,39 @@
+use clap::Parser;
 use std::error::Error;
 
 use log::debug;
-use regex::Regex;
+use regex::{Regex, RegexBuilder};
 
 type MyResult<T> = Result<T, Box<dyn Error>>;
+
+#[derive(Debug, Parser)]
+#[command(about, version, author, long_about=None)]
+struct Args {
+    #[arg(
+        value_name = "FILE",
+        help = "Prints help information",
+        required = false
+    )]
+    sources: Vec<String>,
+
+    #[arg(
+        short = 'm',
+        long = "pattern",
+        value_name = "PATTERN",
+        help = "Pattern"
+    )]
+    pattern: Option<String>,
+
+    #[arg(short = 's', long = "seed", value_name = "SEED", help = "Random seed")]
+    seed: Option<u64>,
+
+    #[arg(
+        short = 'i',
+        long = "insensitive",
+        help = "Case-insensitive pattern matching"
+    )]
+    insensitive: bool,
+}
 
 #[derive(Debug)]
 pub struct Config {
@@ -13,14 +43,26 @@ pub struct Config {
 }
 
 pub fn get_args() -> MyResult<Config> {
+    let args = Args::parse();
+
+    let pattern = args
+        .pattern
+        .map(|p| {
+            RegexBuilder::new(&p)
+                .case_insensitive(args.insensitive)
+                .build()
+                .map_err(|_| format!("Invalid --pattern \"{}\"", p))
+        })
+        .transpose()?;
+
     Ok(Config {
-        sources: vec!["source1".to_string(), "source2".to_string()],
-        pattern: Some(Regex::new(r"pattern")?),
-        seed: Some(42),
+        sources: args.sources,
+        pattern,
+        seed: args.seed,
     })
 }
 
 pub fn run(config: Config) -> MyResult<()> {
-    debug!("Config: {:?}", config);
+    debug!("Config: {:#?}", config);
     Ok(())
 }
--- workbench/12_fortuner/src/main.rs
+++ workbench/12_fortuner/src/main.rs
@@ -2,7 +2,7 @@ fn main() {
     env_logger::init();
 
     if let Err(e) = fortuner::get_args().and_then(fortuner::run) {
-        eprintln!("Error: {}", e);
+        eprintln!("{}", e);
         std::process::exit(1);
     }
 }
